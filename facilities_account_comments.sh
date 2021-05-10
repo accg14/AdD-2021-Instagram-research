c97fb61d25b215ef362862904a65c15569753b5f
Arturo Collazo <accg14@gmail.com>
2021-05-10 18:39:56 -0300

    Added directory for commented post of gyms

---
+++ facilities_account_comments.sh
@@ -0,0 +1,8 @@
+instagram-scraper mvdrustybox -u anadatos21 -p anadatos21! --media-types none --maximum 30 --media-metadata --comments --profile-metadata;
+instagram-scraper avenidacrossfit -u anadatos21 -p anadatos21! --media-types none --maximum 30 --media-metadata --comments --profile-metadata;
+instagram-scraper crossfitdelsur -u anadatos21 -p anadatos21! --media-types none --maximum 30 --media-metadata --comments --profile-metadata;
+instagram-scraper tekoacrossfit -u anadatos21 -p anadatos21! --media-types none --maximum 30 --media-metadata --comments --profile-metadata
+instagram-scraper acptrainingcenter -u anadatos21 -p anadatos21! --media-types none --maximum 30 --media-metadata --comments --profile-metadata
+instagram-scraper montevideoknockout -u anadatos21 -p anadatos21! --media-types none --maximum 30 --media-metadata --comments --profile-metadata
+instagram-scraper instintocf -u anadatos21 -p anadatos21! --media-types none --maximum 30 --media-metadata --comments --profile-metadata
+instagram-scraper sensefit -u anadatos21 -p anadatos21! --media-types none --maximum 30 --media-metadata --comments --profile-metadata
