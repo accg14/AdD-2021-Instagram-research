b3294f168f8f351e4f365bca3b7b842dff03ac86
Arturo Collazo <accg14@gmail.com>
2021-05-21 01:15:19 -0300

    Add centrality measures

--- influencers/influencers.py
+++ influencers/influencers.py
@@ -1,62 +1,80 @@
 INFLUENCERS_COMMUNITY = {
     'santicomba':
       {
+        'id': 0,
         'total_followers': 33666,
-          'influencers_followers': ['fedesfit', 'gentuli', 'cami.filmel', 'jmseitun', 'jimenaalzogaray._', 'entrenamientopersonalep']
-        },
+        'influencers_followers': ['fedesfit', 'gentuli', 'cami.filmel', 'jmseitun', 'jimenaalzogaray._', 'entrenamientopersonalep']
+      },
     'maitef.fitness':
-        {
+      {
+        'id': 1,
         'total_followers': 3329,
         'influencers_followers': []
       },
     'ivi_quirog':
       {
+        'id': 2,
         'total_followers': 8407,
         'influencers_followers': ['maguitolosa1']
       },
     'entrenamiento_trouville':
       {
+        'id': 3,
         'total_followers': 2010,
         'influencers_followers': ['cami.filmel', 'ivi_quirog']
       },
     'fedesfit':
       {
+        'id': 4,
         'total_followers': 2336,
         'influencers_followers': []
       },
     'maguitolosa1':
       {
+        'id': 5,
         'total_followers': 2442,
         'influencers_followers': ['jmseitun', 'entrenamientopersonalep', 'ivi_quirog']
       },
     'fit_person':
-        {
+      {
+        'id': 6,
         'total_followers': 42300,
         'influencers_followers': []
       },
     'gentuli':
-        {
+      {
+        'id': 7,
         'total_followers': 6503,
         'influencers_followers': ['vico_frascolla', 'cami.filmel', 'santicomba', 'fedesfit']
       },
     'jimenaalzogaray._':
-        {
+      {
+        'id': 8,
         'total_followers': 14600,
         'influencers_followers': ['jmseitun', 'fit_person', 'entrenamientopersonalep']
       },
     'jmseitun':
       {
+        'id': 9,
         'total_followers': 5415,
-        'influencers_followers': ['fedesfit', 'santicomba', 'cami.filmel', 'maguitolosa1', 'vicofrascolla', 'jimenaalzogaray._', 'entrenamientopersonalep']
+        'influencers_followers': ['fedesfit', 'santicomba', 'cami.filmel', 'maguitolosa1', 'vico_frascolla', 'jimenaalzogaray._', 'entrenamientopersonalep']
       },
     'vico_frascolla':
       {
+        'id': 10,
         'total_followers': 6185,
         'influencers_followers': ['jmseitun', 'gentuli', 'jimenaalzogaray._', 'maguitolosa1']
       },
     'cami.filmel':
       {
+        'id': 11,
         'total_followers': 65500,
         'influencers_followers': ['santicomba', 'jmseitun', 'maguitolosa1', 'jimenaalzogaray._', 'entrenamientopersonalep', 'gentuli']
+      },
+    'entrenamientopersonalep':
+      {
+        'id': 12,
+        'total_followers': 1696,
+        'influencers_followers': []
       }
   }
