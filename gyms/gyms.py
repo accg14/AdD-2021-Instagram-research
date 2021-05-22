e81535347197310ea9d9c190be06dc285f6b85c2
Arturo Collazo <accg14@gmail.com>
2021-05-22 18:56:46 -0300

    gyms notebooks with dict and sections completed

--- gyms/gyms.py
+++ gyms/gyms.py
@@ -1,35 +1,34 @@
-GYMS:
-{
+GYMS= {
   'acptrainingcenter':
   {
-    'path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources'
+    'file_path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources/acptrainingcenter/acptrainingcenter.json'
   },
   'avenidacrossfit':
   {
-
+    'file_path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources/avenidacrossfit/avenidacrossfit.json'
   },
   'crossfitdelsur':
   {
-
+    'file_path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources/crossfitdelsur/crossfitdelsur.json'
   },
   'instintocf':
   {
-
+    'file_path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources/instintocf/instintocf.json'
   },
   'montevideoknockout':
   {
-
+    'file_path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources/montevideoknockout/montevideoknockout.json'
   },
   'mvdrustybox':
   {
-
+    'file_path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources/mvdrustybox/mvdrustybox.json'
   },
   'sensefit':
   {
-
+    'file_path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources/sensefit/sensefit.json'
   },
   'tekoacrossfit':
   {
-
+    'file_path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources/tekoacrossfit/tekoacrossfit.json'
   }
 }
