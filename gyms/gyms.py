f9a905fd379ca8d5b93dcf0987f753cb229f6073
Arturo Collazo <accg14@gmail.com>
2021-05-22 15:37:27 -0300

    Add sections to influencers

---
+++ gyms/gyms.py
@@ -0,0 +1,35 @@
+GYMS:
+{
+  'acptrainingcenter':
+  {
+    'path': '/home/accg14/Documents/FIng/Taller ARS/Entrega/AdD-2021-Instagram-research/gyms/sources'
+  },
+  'avenidacrossfit':
+  {
+
+  },
+  'crossfitdelsur':
+  {
+
+  },
+  'instintocf':
+  {
+
+  },
+  'montevideoknockout':
+  {
+
+  },
+  'mvdrustybox':
+  {
+
+  },
+  'sensefit':
+  {
+
+  },
+  'tekoacrossfit':
+  {
+
+  }
+}
